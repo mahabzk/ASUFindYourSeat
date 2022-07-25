7b33b23e42c534ea78a5741432e2d9919d2ad3e3
mahabzk <83807132+mahabzk@users.noreply.github.com>
2022-07-25 13:08:45 -0700

    Update keys.py
    changed auth key

--- Scripts/keys.py
+++ Scripts/keys.py
@@ -1,4 +1,4 @@
 account_sid = 'AC3b1634f6568f92de4aa06cb113d03b7c'
-auth_token = 'd588ad9ba458166ef1fefade7c532d6b'
+auth_token = '730f640073f6f0838bd421bb14c852fd'
 
 twilio_number = '+12282310847'
